f99391e87f02d5743fff3d6d6c85e46f0826fafe
Shaun Limbeek <soccercomet@yahoo.com>
2021-02-02 19:09:00 -0800

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+var connection;
 // for local testing
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "movies_DB"
-});
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "movies_DB"
+  });
+}
+ 
 
 // for Heroku
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // Make connection.
 connection.connect(function (err) {
